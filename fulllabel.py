0cb206e02f2355d321f3281535d3965d0e4f9a8e
yeyeyeping <82635395@qq.com>
2023-04-16 13:36:38 +0800

    full label learning

--- fulllabel.py
+++ fulllabel.py
@@ -29,7 +29,7 @@ from util.trainer import BaseTrainer
 def parse_arg():
     parser = ArgumentParser()
     parser.add_argument("--data-dir", type=str,
-                        default="dataset/preprocess")
+                        default="data")
     parser.add_argument("--output-dir", type=str, default="output")
     parser.add_argument("--device", type=str, default="cuda:0")
     parser.add_argument("--model", type=str, default="unet")
@@ -38,14 +38,14 @@ def parse_arg():
     parser.add_argument("--budget", type=int, default=2000)
     parser.add_argument("--query", type=int, default=200)
     parser.add_argument("--batch-size", type=int, default=32)
-    parser.add_argument("--epoch", type=int, default=50)
+    parser.add_argument("--epoch", type=int, default=5)
     parser.add_argument("--num-workers", type=int, default=4)
     parser.add_argument("--lr", type=float, default=1e-4)
     parser.add_argument("--weight-decay", type=float, default=1e-4)
     parser.add_argument("--input-size", type=int, default=416)
     parser.add_argument("--forget-weight", type=bool, default=False)
     parser.add_argument("--query-strategy", type=str, default="LeastConfidence")
-    parser.add_argument("--query-strategy-param", type=dict, default={"round": 10,"pool_size":8})
+    parser.add_argument("--query-strategy-param", type=dict, default={"round": 10, "pool_size": 8})
     parser.add_argument("--trainer-param", type=dict, default={"num_augmentations": 3})
     args = parser.parse_args()
     if not os.path.exists(args.output_dir):
@@ -73,23 +73,6 @@ def init_logger(args):
     return logger
 
 
-def get_samplers(data_num, initial_labeled):
-    data_indice = list(range(data_num))
-    np.random.shuffle(data_indice)
-    return SubsetSampler(data_indice[:initial_labeled]), SubsetSampler(data_indice[initial_labeled:])
-
-
-def save_query_plot(folder, labeled_percent, dice_list):
-    with open(f"{folder}/result.txt", "w") as fp:
-        fp.write("x:")
-        fp.write(str(labeled_percent))
-        fp.write("\ny:")
-        fp.write(str(dice_list))
-    plt.plot(labeled_percent, dice_list)
-    plt.savefig(f"{folder}/result.jpg")
-
-
-
 def get_dataloader(args):
     train_transform = A.Compose([
         A.PadIfNeeded(512, 512),
@@ -102,10 +85,9 @@ def get_dataloader(args):
     dataset_train, dataset_val = Dataset2d(datafolder=os.path.join(args.data_dir, "train"),
                                            transform=train_transform), Dataset3d(
         folder=os.path.join(args.data_dir, "val"))
-    labeled_sampler, unlabeled_sampler = get_samplers(len(dataset_train), args.initial_labeled)
+
     dulabeled = torch.utils.data.DataLoader(dataset_train,
                                             batch_size=args.batch_size,
-                                            sampler=unlabeled_sampler,
                                             persistent_workers=True,
                                             pin_memory=True,
                                             prefetch_factor=4,
@@ -113,7 +95,6 @@ def get_dataloader(args):
 
     dlabeled = torch.utils.data.DataLoader(dataset_train,
                                            batch_size=args.batch_size,
-                                           sampler=labeled_sampler,
                                            persistent_workers=True,
                                            pin_memory=True,
                                            prefetch_factor=4,
@@ -136,69 +117,14 @@ def al_cycle(args, logger):
     writer = SummaryWriter(join(args.output_dir, "tensorboard"))
     dataloader = get_dataloader(args)
 
-    logger.info(
-        f'Initial configuration: len(du): {len(dataloader["unlabeled"].sampler.indices)} '
-        f'len(dl): {len(dataloader["labeled"].sampler.indices)} ')
+    trainer = BaseTrainer(args, logger, writer, args.trainer_param)
+    for i in range(40):
+        loss, iou, dice = trainer.train(dataloader["labeled"], args.epoch, -1)
 
-    strategy_type, trainer_type = build_strategy(args.query_strategy)
-    trainer = trainer_type(args, logger, writer, args.trainer_param)
-
-    query_strategy = strategy_type(dataloader["unlabeled"], dataloader["labeled"], trainer=trainer,
-                                   **args.query_strategy_param)
-
-    loss, iou, dice = trainer.train(dataloader["labeled"], args.epoch, -1)
-    logger.info(
-        f"initial model TRAIN | avg_loss: {loss} Dice:{dice} Mean IoU: {iou} ")
-
-    # validation
-    dice, meaniou, assd = trainer.valid(dataloader["val"], -1, args.batch_size, args.input_size)
-    logger.info(
-        f"initial model EVAl | Dice:{dice} Mean IoU: {meaniou} assd: {assd} ")
-
-    num_dataset = len(dataloader["labeled"].dataset)
-    labeled_percent, dice_list = [], []
-    labeled_percent.append(len(dataloader["labeled"].sampler.indices) / num_dataset)
-    dice_list.append(dice)
-    cycle = 0
-    budget = args.budget
-    query = args.query
-
-    total_cycle = (budget // query) + 1
-    while budget > 0:
-        logger.info(f"cycle {cycle} | budget : {budget} query : {query}")
-
-        if query > budget:
-            query = budget
-        budget -= query
-        cycle += 1
-
-        query_strategy.sample(query)
-
-        logger.info(
-            f'add {query} samplers to labeled dataset')
-
-        if args.forget_weight:
-            logger.info("forget weight")
-            # reset model
-            trainer.forget_weight(cycle, total_cycle)
-
-        # retrain model on updated dataloader
-        loss, iou, dice = trainer.train(dataloader["labeled"], args.epoch, cycle)
-        logger.info(
-            f"CYCLE {cycle} TRAIN | avg_loss: {loss} avg_dice:{dice} avg_mean_iou: {iou} ")
-
-        dice, meaniou, assd = trainer.valid(dataloader["val"], cycle, args.batch_size, args.input_size)
-        logger.info(
-            f'Cycle {cycle} EVAl | len(dl): {len(dataloader["labeled"].sampler.indices)} len(du): {len(dataloader["unlabeled"].sampler.indices)} |  avg_dice:{dice} avg_mean_iou: {meaniou} avg_assd: {assd} ')
-
-        labeled_percent.append(np.round(len(dataloader["labeled"].sampler.indices) / num_dataset, 4))
-        dice_list.append(np.round(dice, 4))
-
-        # save checkpoint
-        torch.save(trainer.model.state_dict(), f"{args.checkpoint}/cycle={cycle}&dice={dice:.3f}&time={time.time()}.pth")
-        if len(dataloader["unlabeled"].sampler.indices) == 0:
-            break
-    save_query_plot(args.output_dir, labeled_percent, dice_list)
+        logger.info(f"model TRAIN | avg_loss: {loss} Dice:{dice} Mean IoU: {iou} ")
+        # validation
+        dice, meaniou, assd = trainer.valid(dataloader["val"], -1, args.batch_size, args.input_size)
+        logger.info(f"model EVAl | Dice:{dice} Mean IoU: {meaniou} assd: {assd} ")
     writer.flush()
     writer.close()
 
