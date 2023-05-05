4a3407d13b5c9e3822e3b84b84df8962d86a9955
yeyeyeping <82635195@qq.com>
2023-05-05 12:54:49 +0800

    bug fixed:weight device

--- util/jitfunc.py
+++ util/jitfunc.py
@@ -3,23 +3,31 @@ import torch
 
 @torch.jit.script
 def margin_confidence(model_output: torch.Tensor, weight=torch.Tensor([1])) -> torch.Tensor:
+    if weight.device != model_output.device:
+        weight = weight.to(model_output.device)
     weight_socre = torch.abs(model_output[:, 0] - model_output[:, 1]) * weight
     return weight_socre.mean(dim=(-1, -2))
 
 
 @torch.jit.script
 def least_confidence(model_output: torch.Tensor, weight=torch.Tensor([1])) -> torch.Tensor:
+    if weight.device != model_output.device:
+        weight = weight.to(model_output.device)
     output_max = torch.max(model_output, dim=1)[0] * weight
     return output_max.mean(dim=(-2, -1))
 
 
 @torch.jit.script
 def max_entropy(model_output: torch.Tensor, weight=torch.Tensor([1])) -> torch.Tensor:
+    if weight.device != model_output.device:
+        weight = weight.to(model_output.device)
     weight_score = -model_output * torch.log(model_output + 1e-7)
     return torch.mean(weight_score.mean(1) * weight, dim=(-2, -1))
 
 
 def hisgram_entropy(model_output: torch.Tensor, weight=torch.Tensor([1])):
+    if weight.device != model_output.device:
+        weight = weight.to(model_output.device)
     score = []
     for output in weight:
         frequency, _ = torch.histogram(output, bins=10)
@@ -42,5 +50,5 @@ def JSD(data: torch.Tensor) -> torch.Tensor:
 if __name__ == '__main__':
     import numpy as np
 
-    a, b = torch.randn(size=(16, 2, 16, 16)), torch.randn(size=(16, 16, 16))
+    a, b = torch.randn(size=(16, 2, 16, 16)), torch.randn(size=(16, 16, 16),device="cuda")
     print(hisgram_entropy(a.softmax(1), b))
