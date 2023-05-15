34ed82ef79c213d7ad61e94f9f197d0f40b255e4
yeyeyeping <82635195@qq.com>
2023-05-15 18:02:33 +0800

    update

--- train.py
+++ train.py
@@ -34,7 +34,7 @@ def parse_arg():
     parser.add_argument("--forget-weight", type=bool, default=False)
     parser.add_argument("--query-strategy", type=str, default="LeastConfidence")
     parser.add_argument("--query-strategy-param", type=dict,
-                        default={"round": 10, "distance_measure": "manhattan", "pool_size": 8,
+                        default={"round": 10, "distance_measure": "cosine", "pool_size": 8,
                                  "constrative_sampler_size": 20, "difficulty_strategy": "max_entropy"})
     parser.add_argument("--trainer-param", type=dict, default={"num_augmentations": 3})
     args = parser.parse_args()
